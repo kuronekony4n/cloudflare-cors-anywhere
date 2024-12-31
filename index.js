dd27af2cf197a1ff7d6b378c0598e0cddf074e15
0willy <38156932+kuronekony4n@users.noreply.github.com>
2024-12-31 19:25:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-/*
-CORS Anywhere as a Cloudflare Worker!s
-(c) 2019 by Zibri (www.zibri.org)
-email: zibri AT zibri DOT org
-https://github.com/Zibri/cloudflare-cors-anywhere
+const blacklistUrls = [];
+const whitelistOrigins = [ ".*" ];
 
-This Cloudflare Worker script acts as a CORS proxy that allows
-cross-origin resource sharing for specified origins and URLs.
-It handles OPTIONS preflight requests and modifies response headers accordingly to enable CORS.
-The script also includes functionality to parse custom headers and provide detailed information
-about the CORS proxy service when accessed without specific parameters.
-The script is configurable with whitelist and blacklist patterns, although the blacklist feature is currently unused.
-The main goal is to facilitate cross-origin requests while enforcing specific security and rate-limiting policies.
-*/
-
-// Configuration: Whitelist and Blacklist (not used in this version)
-// whitelist = [ "^http.?://www.zibri.org$", "zibri.org$", "test\\..*" ];  // regexp for whitelisted urls
-const blacklistUrls = [];           // regexp for blacklisted urls
-const whitelistOrigins = [ ".*" ];   // regexp for whitelisted origins
-
-// Function to check if a given URI or origin is listed in the whitelist or blacklist
 function isListedInWhitelist(uri, listing) {
     let isListed = false;
     if (typeof uri === "string") {
@@ -28,43 +10,35 @@ function isListedInWhitelist(uri, listing) {
             }
         });
     } else {
-        // When URI is null (e.g., when Origin header is missing), decide based on the implementation
-        isListed = true; // true accepts null origins, false would reject them
+        isListed = true;
     }
     return isListed;
 }
 
-// Event listener for incoming fetch requests
 addEventListener("fetch", async event => {
     event.respondWith((async function() {
         const isPreflightRequest = (event.request.method === "OPTIONS");
-        
         const originUrl = new URL(event.request.url);
 
-        // Function to modify headers to enable CORS
         function setupCORSHeaders(headers) {
             headers.set("Access-Control-Allow-Origin", event.request.headers.get("Origin"));
             if (isPreflightRequest) {
                 headers.set("Access-Control-Allow-Methods", event.request.headers.get("access-control-request-method"));
                 const requestedHeaders = event.request.headers.get("access-control-request-headers");
-
                 if (requestedHeaders) {
                     headers.set("Access-Control-Allow-Headers", requestedHeaders);
                 }
-
-                headers.delete("X-Content-Type-Options"); // Remove X-Content-Type-Options header
+                headers.delete("X-Content-Type-Options");
             }
             return headers;
         }
 
         const targetUrl = decodeURIComponent(decodeURIComponent(originUrl.search.substr(1)));
-
         const originHeader = event.request.headers.get("Origin");
         const connectingIp = event.request.headers.get("CF-Connecting-IP");
 
         if ((!isListedInWhitelist(targetUrl, blacklistUrls)) && (isListedInWhitelist(originHeader, whitelistOrigins))) {
             let customHeaders = event.request.headers.get("x-cors-headers");
-
             if (customHeaders !== null) {
                 try {
                     customHeaders = JSON.parse(customHeaders);
@@ -95,7 +69,7 @@ addEventListener("fetch", async event => {
                 });
 
                 const response = await fetch(targetUrl, newRequest);
-                const responseHeaders = new Headers(response.headers);
+                let responseHeaders = new Headers(response.headers);
                 const exposedHeaders = [];
                 const allResponseHeaders = {};
                 for (const [key, value] of response.headers.entries()) {
@@ -118,7 +92,7 @@ addEventListener("fetch", async event => {
                 return new Response(responseBody, responseInit);
 
             } else {
-                const responseHeaders = new Headers();
+                let responseHeaders = new Headers();
                 responseHeaders = setupCORSHeaders(responseHeaders);
 
                 let country = false;
@@ -143,25 +117,4 @@ addEventListener("fetch", async event => {
                     "\n" +
                     (customHeaders !== null ? "\nx-cors-headers: " + JSON.stringify(customHeaders) : ""),
                     {
-                        status: 200,
-                        headers: responseHeaders
-                    }
-                );
-            }
-        } else {
-            return new Response(
-                "Create your own CORS proxy</br>\n" +
-                "<a href='https://github.com/Zibri/cloudflare-cors-anywhere'>https://github.com/Zibri/cloudflare-cors-anywhere</a></br>\n" +
-                "\nDonate</br>\n" +
-                "<a href='https://paypal.me/Zibri/5'>https://paypal.me/Zibri/5</a>\n",
-                {
-                    status: 403,
-                    statusText: 'Forbidden',
-                    headers: {
-                        "Content-Type": "text/html"
-                    }
-                }
-            );
-        }
-    })());
-});
+                        status: 200
